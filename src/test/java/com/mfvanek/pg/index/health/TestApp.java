239a8522a5b34579a72993cf32d48368dd7ef007
ivan.vakhrushev <ivvakhrushev@yandex-team.ru>
2019-12-03 17:58:08 +0300

    Added integrational test

--- src/test/java/com/mfvanek/pg/index/health/TestApp.java
+++ src/test/java/com/mfvanek/pg/index/health/TestApp.java
@@ -5,9 +5,25 @@
 
 package com.mfvanek.pg.index.health;
 
+import com.mfvanek.pg.connection.PgConnection;
+import com.mfvanek.pg.connection.PoolAwarePgConnection;
+import com.mfvanek.pg.index.health.logger.Exclusions;
+import com.mfvanek.pg.index.health.logger.IndicesHealthLogger;
+import com.mfvanek.pg.index.health.logger.SimpleHealthLogger;
+import com.mfvanek.pg.index.maintenance.IndexMaintenanceFactoryImpl;
+
 public class TestApp {
 
     public static void main(String[] args) {
+        // TODO Писать на какой хост пошли с запросом!
+        // TODO Добавить урл для асинхронной реплики в Чекаутере
+        // TODO Надо больше тестов
 
+        final PgConnection pgConnection = PoolAwarePgConnection.of("jdbc:postgresql://vla-di1z5b4nx75yqonp.db.yandex.net:6432,market-checkouter-test01i.db.yandex.net:6432,market-checkouter-test01h.db.yandex.net:6432/market_checkouter_test?targetServerType=master&ssl=true&prepareThreshold=0&preparedStatementCacheQueries=0&sslmode=require",
+                "market_checkouter_ro", "za12Pqo83mcbd10rtyskadv", "jdbc:postgresql://vla-di1z5b4nx75yqonp.db.yandex.net:6432,market-checkouter-test01i.db.yandex.net:6432,market-checkouter-test01h.db.yandex.net:6432/market_checkouter_test?targetServerType=preferSlave&loadBalanceHosts=true&ssl=true&prepareThreshold=0&preparedStatementCacheQueries=0&sslmode=require");
+        final IndicesHealth indicesHealth = new IndicesHealthImpl(pgConnection, new IndexMaintenanceFactoryImpl());
+        final IndicesHealthLogger logger = new SimpleHealthLogger(indicesHealth, Exclusions.empty());
+        System.out.println();
+        logger.logAll().forEach(System.out::println);
     }
 }
