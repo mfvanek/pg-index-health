ef5f6f4be81be5e40ee616e68aa23f114d975d79
ivan.vakhrushev <ivvakhrushev@yandex-team.ru>
2019-12-25 22:53:00 +0300

    Updated copyright

--- src/main/java/io/github/mfvanek/pg/connection/HighAvailabilityPgConnectionImpl.java
+++ src/main/java/io/github/mfvanek/pg/connection/HighAvailabilityPgConnectionImpl.java
@@ -1,6 +1,8 @@
 /*
- * Copyright (c) 2019. Ivan Vakhrushev. All rights reserved.
+ * Copyright (c) 2019. Ivan Vakhrushev.
  * https://github.com/mfvanek
+ *
+ * This file is a part of "pg-index-health" - a Java library for analyzing and maintaining indexes health in Postgresql databases.
  */
 
 package io.github.mfvanek.pg.connection;
