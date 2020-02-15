3a6e7b8d93bc5b15a00af70b49d0b6e3e5ce4587
Ivan Vakhrushev <37612014+mfvanek@users.noreply.github.com>
2020-02-15 17:52:13 +0300

    Updated copyright (#39)

--- src/main/java/io/github/mfvanek/pg/index/health/ReplicasHelper.java
+++ src/main/java/io/github/mfvanek/pg/index/health/ReplicasHelper.java
@@ -1,8 +1,8 @@
 /*
- * Copyright (c) 2019. Ivan Vakhrushev.
- * https://github.com/mfvanek
+ * Copyright (c) 2019-2020. Ivan Vakhrushev and others.
+ * https://github.com/mfvanek/pg-index-health
  *
- * This file is a part of "pg-index-health" - a Java library for analyzing and maintaining indexes health in Postgresql databases.
+ * This file is a part of "pg-index-health" - a Java library for analyzing and maintaining indexes health in PostgreSQL databases.
  */
 
 package io.github.mfvanek.pg.index.health;
