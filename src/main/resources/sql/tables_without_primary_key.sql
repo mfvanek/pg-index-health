81247cd4c92bd804e8775fad7808d8e8b37b65a5
ivan.vakhrushev <ivvakhrushev@yandex-team.ru>
2020-03-09 17:22:57 +0300

    Deleted local sql queries

--- src/main/resources/sql/tables_without_primary_key.sql
+++
@@ -1,9 +0,0 @@
-select psat.relid::regclass::text as table_name,
-    pg_table_size(psat.relid) as table_size
-from pg_catalog.pg_stat_all_tables psat
-where psat.schemaname = :schema_name_param::text
-  and psat.relid::regclass not in (
-    select c.conrelid::regclass as table_name
-    from pg_catalog.pg_constraint c
-    where c.contype = 'p')
-order by psat.relname::text;
