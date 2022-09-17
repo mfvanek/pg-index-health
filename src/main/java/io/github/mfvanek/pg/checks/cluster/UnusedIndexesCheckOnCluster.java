e144fea86d08d47da53dbd93113627fa7b81fd6f
Ivan Vakhrushev <i.vakhrushev@banki.ru>
2022-09-17 20:14:04 +0300

    Making abstract classes package private

--- src/main/java/io/github/mfvanek/pg/checks/cluster/UnusedIndexesCheckOnCluster.java
+++ src/main/java/io/github/mfvanek/pg/checks/cluster/UnusedIndexesCheckOnCluster.java
@@ -11,7 +11,6 @@
 package io.github.mfvanek.pg.checks.cluster;
 
 import io.github.mfvanek.pg.checks.host.UnusedIndexesCheckOnHost;
-import io.github.mfvanek.pg.common.maintenance.AbstractCheckOnCluster;
 import io.github.mfvanek.pg.connection.HighAvailabilityPgConnection;
 import io.github.mfvanek.pg.connection.PgConnection;
 import io.github.mfvanek.pg.connection.PgHost;
