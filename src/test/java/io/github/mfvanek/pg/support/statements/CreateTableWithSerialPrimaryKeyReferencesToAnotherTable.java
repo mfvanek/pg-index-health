1c895c92a8338cf8f3a6e7aeafdc4792cca90eb7
Ivan Vakhrushev <37612014+mfvanek@users.noreply.github.com>
2022-09-24 23:08:57 +0300

    Wrong detection of serial types on columns with multiple constraints (#179)
    * Updated sql query
    
    * Added test to cover the issue
    
    Co-authored-by: Ivan Vakhrushev <i.vakhrushev@banki.ru>

---
+++ src/test/java/io/github/mfvanek/pg/support/statements/CreateTableWithSerialPrimaryKeyReferencesToAnotherTable.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2019-2022. Ivan Vakhrushev and others.
+ * https://github.com/mfvanek/pg-index-health
+ *
+ * This file is a part of "pg-index-health" - a Java library for
+ * analyzing and maintaining indexes health in PostgreSQL databases.
+ *
+ * Licensed under the Apache License 2.0
+ */
+
+package io.github.mfvanek.pg.support.statements;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.annotation.Nonnull;
+
+public class CreateTableWithSerialPrimaryKeyReferencesToAnotherTable extends AbstractDbStatement {
+
+    public CreateTableWithSerialPrimaryKeyReferencesToAnotherTable(@Nonnull final String schemaName) {
+        super(schemaName);
+    }
+
+    @Override
+    public void execute(@Nonnull final Statement statement) throws SQLException {
+        statement.execute(String.format("create table if not exists %1$s.test_table(" +
+                        "id bigserial, " +
+                        "num bigserial, " +
+                        "constraint test_table_pkey_id primary key (id), " +
+                        "constraint test_table_fkey_other_id foreign key (id) references %1$s.another_table (id), " +
+                        "constraint test_table_fkey_one_more_id foreign key (id) references %1$s.one_more_table (id));",
+                schemaName));
+    }
+}
