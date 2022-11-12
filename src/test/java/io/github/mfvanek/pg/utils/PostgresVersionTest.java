4a70509c926d4792d8566a7a5ad5ff894816d916
Ivan Vakhrushev <37612014+mfvanek@users.noreply.github.com>
2022-11-12 23:10:45 +0300

    Switched to PostgreSQL 15.1 and corresponding bitnami container (#187)
    Co-authored-by: Ivan Vakhrushev <i.vakhrushev@banki.ru>

--- src/test/java/io/github/mfvanek/pg/utils/PostgresVersionTest.java
+++ src/test/java/io/github/mfvanek/pg/utils/PostgresVersionTest.java
@@ -31,7 +31,7 @@ final class PostgresVersionTest extends DatabaseAwareTestBase {
     void checkPgVersion() {
         String requiredPgVersionString = System.getenv(PG_VERSION_ENVIRONMENT_VARIABLE);
         if (requiredPgVersionString == null) {
-            requiredPgVersionString = "15.0 (Debian 15.0-";
+            requiredPgVersionString = "15.1 (Debian 15.1-";
         }
         final String actualPgVersionString = readPgVersion();
         assertThat(actualPgVersionString).startsWith(requiredPgVersionString);
