67a2034a8bbd55a63f89c98a7e0861fed5a17a22
Ivan Vakhrushev <i.vakhrushev@banki.ru>
2023-02-11 10:15:23 +0300

    Updating headers

--- src/test/java/io/github/mfvanek/pg/support/statements/CreateClientsTableStatement.java
+++ src/test/java/io/github/mfvanek/pg/support/statements/CreateClientsTableStatement.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2022. Ivan Vakhrushev and others.
+ * Copyright (c) 2019-2023. Ivan Vakhrushev and others.
  * https://github.com/mfvanek/pg-index-health
  *
  * This file is a part of "pg-index-health" - a Java library for
