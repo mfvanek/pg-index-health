3dce971dc7a0482c0aa982827f75617a276921cd
Ivan Vakhrushev <mfvanek@gmail.com>
2024-01-01 14:43:10 +0400

    Remove copyright

--- pg-index-health-testing/build.gradle.kts
+++ pg-index-health-testing/build.gradle.kts
@@ -1,13 +1,3 @@
-/*
- * Copyright (c) 2019-2024. Ivan Vakhrushev and others.
- * https://github.com/mfvanek/pg-index-health
- *
- * This file is a part of "pg-index-health" - a Java library for
- * analyzing and maintaining indexes health in PostgreSQL databases.
- *
- * Licensed under the Apache License 2.0
- */
-
 plugins {
     id("pg-index-health.java-conventions")
     id("pg-index-health.publish")
