f1b45226ab4f689329c241eeb09bf7f0a6b2284b
Ivan Vakhrushev <mfvanek@gmail.com>
2024-04-04 14:16:22 +0400

    Update dependencies

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,8 +7,8 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.spotbugs.snom:spotbugs-gradle-plugin:6.0.6")
-    implementation("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:4.4.1.3373")
+    implementation("com.github.spotbugs.snom:spotbugs-gradle-plugin:6.0.9")
+    implementation("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:5.0.0.4638")
     implementation("net.ltgt.gradle:gradle-errorprone-plugin:3.1.0")
     implementation("info.solidsoft.gradle.pitest:gradle-pitest-plugin:1.15.0")
     implementation("org.gradle:test-retry-gradle-plugin:1.5.8")
