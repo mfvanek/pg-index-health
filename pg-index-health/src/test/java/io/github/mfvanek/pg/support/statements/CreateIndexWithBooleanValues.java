1e31b32d65640ecbf2e87f0b914874383a3e84db
Ivan Vakhrushev <mfvanek@gmail.com>
2024-04-28 13:01:22 +0400

    Add tests

---
+++ pg-index-health/src/test/java/io/github/mfvanek/pg/support/statements/CreateIndexWithBooleanValues.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2019-2024. Ivan Vakhrushev and others.
+ * https://github.com/mfvanek/pg-index-health
+ *
+ * This file is a part of "pg-index-health" - a Java library for
+ * analyzing and maintaining indexes health in PostgreSQL databases.
+ *
+ * Licensed under the Apache License 2.0
+ */
+
+package io.github.mfvanek.pg.support.statements;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.annotation.Nonnull;
+
+public class CreateIndexWithBooleanValues extends AbstractDbStatement {
+
+    public CreateIndexWithBooleanValues(@Nonnull final String schemaName) {
+        super(schemaName);
+    }
+
+    @Override
+    public void execute(@Nonnull final Statement statement) throws SQLException {
+        statement.execute(String.format("create index if not exists i_accounts_deleted " +
+                "on %s.accounts (deleted)", schemaName));
+        statement.execute(String.format("create unique index if not exists i_accounts_account_number_deleted " +
+                "on %s.accounts (account_number, deleted)", schemaName));
+    }
+}
