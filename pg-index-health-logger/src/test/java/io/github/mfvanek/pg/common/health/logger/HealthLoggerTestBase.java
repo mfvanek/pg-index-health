918ec9091ec490d5d4170dda809ce4f650ac0890
Ivan Vakhrushev <mfvanek@gmail.com>
2024-10-17 11:10:14 +0400

    Get rid of duplication in HealthLoggerTest and StandardHealthLoggerTest (#464)
    * Get rid of duplication in HealthLoggerTest and StandardHealthLoggerTest
    
    * Fix test

--- pg-index-health-logger/src/test/java/io/github/mfvanek/pg/common/health/logger/HealthLoggerTestBase.java
+++ pg-index-health-logger/src/test/java/io/github/mfvanek/pg/common/health/logger/HealthLoggerTestBase.java
@@ -10,24 +10,101 @@
 
 package io.github.mfvanek.pg.common.health.logger;
 
+import io.github.mfvanek.pg.common.maintenance.Diagnostic;
+import io.github.mfvanek.pg.connection.HighAvailabilityPgConnectionFactory;
+import io.github.mfvanek.pg.connection.HighAvailabilityPgConnectionFactoryImpl;
+import io.github.mfvanek.pg.connection.PgConnectionFactoryImpl;
+import io.github.mfvanek.pg.connection.PrimaryHostDeterminerImpl;
+import io.github.mfvanek.pg.model.PgContext;
+import io.github.mfvanek.pg.support.DatabaseConfigurer;
 import io.github.mfvanek.pg.support.StatisticsAwareTestBase;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.List;
 import java.util.Objects;
 import java.util.function.Predicate;
 import javax.annotation.Nonnull;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 abstract class HealthLoggerTestBase extends StatisticsAwareTestBase {
 
+    private static final DatabaseConfigurer CONFIGURER = dbp -> dbp.withReferences()
+        .withData()
+        .withInvalidIndex()
+        .withNullValuesInIndex()
+        .withBooleanValuesInIndex()
+        .withTableWithoutPrimaryKey()
+        .withDuplicatedIndex()
+        .withNonSuitableIndex()
+        .withJsonType()
+        .withSerialType()
+        .withFunctions()
+        .withNotValidConstraints()
+        .withBtreeIndexesOnArrayColumn()
+        .withSequenceOverflow()
+        .withDuplicatedForeignKeys()
+        .withIntersectedForeignKeys();
+
     @Nonnull
-    protected static Predicate<String> ofKey(@Nonnull final SimpleLoggingKey key) {
+    protected static Predicate<String> ofKey(@Nonnull final LoggingKey key) {
         return new SimpleLoggingKeyPredicate(key);
+    }
+
+    @Nonnull
+    protected static HighAvailabilityPgConnectionFactory getConnectionFactory() {
+        return new HighAvailabilityPgConnectionFactoryImpl(new PgConnectionFactoryImpl(), new PrimaryHostDeterminerImpl());
+    }
+
+    @Nonnull
+    protected abstract HealthLogger getHealthLogger();
+
+    @Nonnull
+    protected abstract String getExpectedValueForDefaultSchema(@Nonnull LoggingKey key);
+
+    @Nonnull
+    protected abstract String[] getExpectedValue();
+
+    @Test
+    void completenessTest() {
+        assertThat(getHealthLogger().logAll(Exclusions.empty()))
+            .as("All diagnostics must be logged")
+            .hasSameSizeAs(Diagnostic.values());
+    }
+
+    @Test
+    void logAllWithDefaultSchema() {
+        final List<String> logs = getHealthLogger().logAll(Exclusions.empty());
+        assertThat(logs)
+            .hasSameSizeAs(Diagnostic.values());
+        for (final LoggingKey key : SimpleLoggingKey.values()) {
+            assertThat(logs)
+                .filteredOn(ofKey(key))
+                .hasSize(1)
+                .containsExactly(getExpectedValueForDefaultSchema(key));
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {PgContext.DEFAULT_SCHEMA_NAME, "custom"})
+    void logAll(final String schemaName) {
+        executeTestOnDatabase(schemaName, CONFIGURER,
+            ctx -> {
+                collectStatistics(schemaName);
+                assertThat(getHealthLogger().logAll(Exclusions.empty(), ctx))
+                    .hasSameSizeAs(Diagnostic.values())
+                    .containsExactlyInAnyOrder(getExpectedValue());
+            }
+        );
     }
 
     private static class SimpleLoggingKeyPredicate implements Predicate<String> {
 
-        private final SimpleLoggingKey key;
+        private final LoggingKey key;
 
-        SimpleLoggingKeyPredicate(@Nonnull final SimpleLoggingKey key) {
+        SimpleLoggingKeyPredicate(@Nonnull final LoggingKey key) {
             this.key = Objects.requireNonNull(key);
         }
 
--- pg-index-health-logger/src/test/java/io/github/mfvanek/pg/common/health/logger/StandardHealthLoggerTest.java
+++ pg-index-health-logger/src/test/java/io/github/mfvanek/pg/common/health/logger/StandardHealthLoggerTest.java
@@ -11,86 +11,49 @@
 package io.github.mfvanek.pg.common.health.logger;
 
 import io.github.mfvanek.pg.common.maintenance.DatabaseChecks;
-import io.github.mfvanek.pg.common.maintenance.Diagnostic;
-import io.github.mfvanek.pg.connection.HighAvailabilityPgConnectionFactoryImpl;
-import io.github.mfvanek.pg.connection.PgConnectionFactoryImpl;
-import io.github.mfvanek.pg.connection.PrimaryHostDeterminerImpl;
-import io.github.mfvanek.pg.model.PgContext;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import javax.annotation.Nonnull;
 
 class StandardHealthLoggerTest extends HealthLoggerTestBase {
 
-    private final HealthLogger logger = new StandardHealthLogger(
-        getConnectionCredentials(),
-        new HighAvailabilityPgConnectionFactoryImpl(new PgConnectionFactoryImpl(), new PrimaryHostDeterminerImpl()),
-        DatabaseChecks::new);
-
-    @ParameterizedTest
-    @ValueSource(strings = {PgContext.DEFAULT_SCHEMA_NAME, "custom"})
-    void logAll(final String schemaName) {
-        executeTestOnDatabase(schemaName,
-            dbp -> dbp.withReferences()
-                .withData()
-                .withInvalidIndex()
-                .withNullValuesInIndex()
-                .withBooleanValuesInIndex()
-                .withBtreeIndexesOnArrayColumn()
-                .withTableWithoutPrimaryKey()
-                .withDuplicatedIndex()
-                .withNonSuitableIndex()
-                .withJsonType()
-                .withSerialType()
-                .withFunctions()
-                .withNotValidConstraints()
-                .withDuplicatedForeignKeys()
-                .withIntersectedForeignKeys(),
-            ctx -> {
-                collectStatistics(schemaName);
-                final List<String> logs = logger.logAll(Exclusions.empty(), ctx);
-                assertThat(logs)
-                    .hasSameSizeAs(Diagnostic.values())
-                    .containsExactlyInAnyOrder(
-                        "invalid_indexes:1",
-                        "duplicated_indexes:2",
-                        "foreign_keys_without_index:5",
-                        "tables_without_primary_key:2",
-                        "indexes_with_null_values:1",
-                        "indexes_with_bloat:17",
-                        "tables_with_bloat:2",
-                        "intersected_indexes:11",
-                        "unused_indexes:12",
-                        "tables_with_missing_indexes:0",
-                        "tables_without_description:5",
-                        "columns_without_description:23",
-                        "columns_with_json_type:1",
-                        "columns_with_serial_types:2",
-                        "functions_without_description:2",
-                        "indexes_with_boolean:1",
-                        "not_valid_constraints:2",
-                        "btree_indexes_on_array_columns:2",
-                        "sequence_overflow:0",
-                        "primary_keys_with_serial_types:1",
-                        "duplicated_foreign_keys:3",
-                        "intersected_foreign_keys:1");
-            });
+    @Nonnull
+    @Override
+    protected HealthLogger getHealthLogger() {
+        return new StandardHealthLogger(getConnectionCredentials(), getConnectionFactory(), DatabaseChecks::new);
     }
 
-    @Test
-    void logAllWithDefaultSchema() {
-        final List<String> logs = logger.logAll(Exclusions.empty());
-        assertThat(logs)
-            .hasSameSizeAs(Diagnostic.values());
-        for (final SimpleLoggingKey key : SimpleLoggingKey.values()) {
-            assertThat(logs)
-                .filteredOn(ofKey(key))
-                .hasSize(1)
-                .containsExactly(key.getSubKeyName() + ":0");
-        }
+    @Nonnull
+    @Override
+    protected String[] getExpectedValue() {
+        return new String[]{
+            "invalid_indexes:1",
+            "duplicated_indexes:2",
+            "foreign_keys_without_index:5",
+            "tables_without_primary_key:2",
+            "indexes_with_null_values:1",
+            "indexes_with_bloat:17",
+            "tables_with_bloat:2",
+            "intersected_indexes:11",
+            "unused_indexes:12",
+            "tables_with_missing_indexes:0",
+            "tables_without_description:5",
+            "columns_without_description:23",
+            "columns_with_json_type:1",
+            "columns_with_serial_types:2",
+            "functions_without_description:2",
+            "indexes_with_boolean:1",
+            "not_valid_constraints:2",
+            "btree_indexes_on_array_columns:2",
+            "sequence_overflow:3",
+            "primary_keys_with_serial_types:1",
+            "duplicated_foreign_keys:3",
+            "intersected_foreign_keys:1"
+        };
+    }
+
+    @Nonnull
+    @Override
+    protected String getExpectedValueForDefaultSchema(@Nonnull final LoggingKey key) {
+        return key.getSubKeyName() + ":0";
     }
 }
