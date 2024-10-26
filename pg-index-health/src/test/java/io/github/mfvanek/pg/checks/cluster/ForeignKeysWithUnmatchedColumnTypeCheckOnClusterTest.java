ca1177d082d31c8493fcbf90d1ec10b7132eb220
Ivan Vakhrushev <mfvanek@gmail.com>
2024-10-27 00:23:16 +0400

    Add new check

---
+++ pg-index-health/src/test/java/io/github/mfvanek/pg/checks/cluster/ForeignKeysWithUnmatchedColumnTypeCheckOnClusterTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2019-2024. Ivan Vakhrushev and others.
+ * https://github.com/mfvanek/pg-index-health
+ *
+ * This file is a part of "pg-index-health" - a Java library for
+ * analyzing and maintaining indexes health in PostgreSQL databases.
+ *
+ * Licensed under the Apache License 2.0
+ */
+
+package io.github.mfvanek.pg.checks.cluster;
+
+import io.github.mfvanek.pg.common.maintenance.DatabaseCheckOnCluster;
+import io.github.mfvanek.pg.common.maintenance.Diagnostic;
+import io.github.mfvanek.pg.model.PgContext;
+import io.github.mfvanek.pg.model.column.Column;
+import io.github.mfvanek.pg.model.constraint.ForeignKey;
+import io.github.mfvanek.pg.support.DatabaseAwareTestBase;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static io.github.mfvanek.pg.support.AbstractCheckOnClusterAssert.assertThat;
+
+class ForeignKeysWithUnmatchedColumnTypeCheckOnClusterTest extends DatabaseAwareTestBase {
+
+    private final DatabaseCheckOnCluster<ForeignKey> check = new ForeignKeysWithUnmatchedColumnTypeCheckOnCluster(getHaPgConnection());
+
+    @Test
+    void shouldSatisfyContract() {
+        assertThat(check)
+            .hasType(ForeignKey.class)
+            .hasDiagnostic(Diagnostic.FOREIGN_KEYS_WITH_UNMATCHED_COLUMN_TYPE)
+            .isStatic();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {PgContext.DEFAULT_SCHEMA_NAME, "custom"})
+    void onDatabaseWithThem(final String schemaName) {
+        executeTestOnDatabase(schemaName, dbp -> dbp.withReferences().withForeignKeyOnNullableColumn(), ctx -> {
+            final String badClientsTableName = ctx.enrichWithSchema("bad_clients");
+            assertThat(check)
+                .executing(ctx)
+                .hasSize(2)
+                .containsExactlyInAnyOrder(
+                    ForeignKey.ofColumn(badClientsTableName, "c_bad_clients_fk_real_client_id",
+                        Column.ofNullable(badClientsTableName, "real_client_id")),
+                    ForeignKey.ofColumn(badClientsTableName, "c_bad_clients_fk_email_phone",
+                        Column.ofNullable(badClientsTableName, "phone"))
+                );
+        });
+    }
+}
