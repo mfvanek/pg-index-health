825f13c0d1dbd4b40bda83be4acf6e3c7237c1a6
Ivan Vakhrushev <mfvanek@gmail.com>
2024-12-09 18:37:30 +0400

    Remove unnecessary code

--- pg-index-health-core/src/main/java/io/github/mfvanek/pg/core/checks/extractors/ColumnWithSerialTypeExtractor.java
+++ pg-index-health-core/src/main/java/io/github/mfvanek/pg/core/checks/extractors/ColumnWithSerialTypeExtractor.java
@@ -14,16 +14,12 @@ import io.github.mfvanek.pg.core.checks.common.ResultSetExtractor;
 import io.github.mfvanek.pg.model.column.Column;
 import io.github.mfvanek.pg.model.column.ColumnWithSerialType;
 import io.github.mfvanek.pg.model.column.SerialType;
-import io.github.mfvanek.pg.model.context.PgContext;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Locale;
 import javax.annotation.Nonnull;
 
 public class ColumnWithSerialTypeExtractor implements ResultSetExtractor<ColumnWithSerialType> {
-
-    private static final String PUBLIC_SCHEMA_PREFIX = PgContext.DEFAULT_SCHEMA_NAME + '.';
 
     private final ResultSetExtractor<Column> columnExtractor;
 
@@ -40,20 +36,7 @@ public class ColumnWithSerialTypeExtractor implements ResultSetExtractor<ColumnW
         final Column column = columnExtractor.extractData(resultSet);
         final String columnType = resultSet.getString("column_type");
         final String sequenceName = resultSet.getString("sequence_name");
-        return ColumnWithSerialType.of(column, SerialType.valueFrom(columnType), normalizeSequenceName(sequenceName));
-    }
-
-    // Issue here is that pg_catalog.pg_sequence.seqrelid::regclass returns sequence name without 'public' schema prefix
-    // but function 'pg_get_serial_sequence' does.
-    //
-    // See https://www.postgresql.org/docs/current/catalog-pg-sequence.html
-    // See https://www.postgresql.org/docs/current/functions-info.html
-    @Nonnull
-    private static String normalizeSequenceName(@Nonnull final String sequenceName) {
-        if (sequenceName.toLowerCase(Locale.ROOT).startsWith(PUBLIC_SCHEMA_PREFIX)) {
-            return sequenceName.substring(PUBLIC_SCHEMA_PREFIX.length());
-        }
-        return sequenceName;
+        return ColumnWithSerialType.of(column, SerialType.valueFrom(columnType), sequenceName);
     }
 
     /**
