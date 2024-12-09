fd91998d0f6fa9df7f5fdcca9a1b4fab4c47ab75
Ivan Vakhrushev <mfvanek@gmail.com>
2024-12-09 18:14:47 +0400

    Fix sql

--- spring-boot-integration/postgres-demo-app-with-custom-user/src/main/resources/init.sql
+++ spring-boot-integration/postgres-demo-app-with-custom-user/src/main/resources/init.sql
@@ -1,13 +1,3 @@
-/*
- * Copyright (c) 2019-2024. Ivan Vakhrushev and others.
- * https://github.com/mfvanek/pg-index-health
- *
- * This file is a part of "pg-index-health" - a Java library for
- * analyzing and maintaining indexes health in PostgreSQL databases.
- *
- * Licensed under the Apache License 2.0
- */
-
 create schema if not exists main_schema authorization main_user;
 create schema if not exists additional_schema authorization pg_database_owner;
 
