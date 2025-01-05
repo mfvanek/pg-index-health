d565547b296ad533da9e65410201669fd8976e1a
Ivan Vakhrushev <mfvanek@gmail.com>
2025-01-05 20:00:07 +0400

    Update copyright #2

--- spring-boot-integration/console-demo-app/build.gradle.kts
+++ spring-boot-integration/console-demo-app/build.gradle.kts
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2019-2025. Ivan Vakhrushev and others.
+ * https://github.com/mfvanek/pg-index-health
+ *
+ * This file is a part of "pg-index-health" - a Java library for
+ * analyzing and maintaining indexes health in PostgreSQL databases.
+ *
+ * Licensed under the Apache License 2.0
+ */
+
 plugins {
     id("pg-index-health.java-application")
     alias(libs.plugins.spring.boot.gradlePlugin)
