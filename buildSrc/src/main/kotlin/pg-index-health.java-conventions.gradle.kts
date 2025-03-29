5b272237e510f93774e80f7c00cf59457faec47b
Ivan Vakhrushev <mfvanek@gmail.com>
2025-03-29 22:43:17 +0400

    Fix checkstyle

--- buildSrc/src/main/kotlin/pg-index-health.java-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/pg-index-health.java-conventions.gradle.kts
@@ -44,7 +44,7 @@ tasks.withType<JavaCompile>().configureEach {
 
 tasks {
     test {
-        dependsOn(checkstyleMain, checkstyleTest, pmdMain, pmdTest, spotbugsMain, spotbugsTest)
+        dependsOn(checkstyleMain, checkstyleTest, checkstyleTestFixtures, pmdMain, pmdTest, spotbugsMain, spotbugsTest)
     }
 
     withType<Test>().configureEach {
