e0424b0248d96b30fde7d564ebc8a08deb03e39c
Ivan Vakhrushev <mfvanek@gmail.com>
2025-04-12 22:58:57 +0400

    [not_valid_constraints] Add test with badly named constraints

--- pg-index-health-logger/src/test/java/io/github/mfvanek/pg/health/logger/HealthLoggerTest.java
+++ pg-index-health-logger/src/test/java/io/github/mfvanek/pg/health/logger/HealthLoggerTest.java
@@ -70,7 +70,7 @@ class HealthLoggerTest extends HealthLoggerTestBase {
             "1999-12-31T23:59:59Z\tdb_indexes_health\tcolumns_with_serial_types\t3",
             "1999-12-31T23:59:59Z\tdb_indexes_health\tfunctions_without_description\t3",
             "1999-12-31T23:59:59Z\tdb_indexes_health\tindexes_with_boolean\t1",
-            "1999-12-31T23:59:59Z\tdb_indexes_health\tnot_valid_constraints\t2",
+            "1999-12-31T23:59:59Z\tdb_indexes_health\tnot_valid_constraints\t3",
             "1999-12-31T23:59:59Z\tdb_indexes_health\tbtree_indexes_on_array_columns\t2",
             "1999-12-31T23:59:59Z\tdb_indexes_health\tsequence_overflow\t3",
             "1999-12-31T23:59:59Z\tdb_indexes_health\tprimary_keys_with_serial_types\t2",
--- pg-index-health-logger/src/test/java/io/github/mfvanek/pg/health/logger/StandardHealthLoggerTest.java
+++ pg-index-health-logger/src/test/java/io/github/mfvanek/pg/health/logger/StandardHealthLoggerTest.java
@@ -42,7 +42,7 @@ class StandardHealthLoggerTest extends HealthLoggerTestBase {
             "columns_with_serial_types:3",
             "functions_without_description:3",
             "indexes_with_boolean:1",
-            "not_valid_constraints:2",
+            "not_valid_constraints:3",
             "btree_indexes_on_array_columns:2",
             "sequence_overflow:3",
             "primary_keys_with_serial_types:2",
