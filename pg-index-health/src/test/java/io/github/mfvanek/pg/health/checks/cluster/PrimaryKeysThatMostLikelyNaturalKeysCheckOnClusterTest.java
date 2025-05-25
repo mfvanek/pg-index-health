504e3843ff9e8fe8da24331bd3be6e3d52ea5843
Ivan Vakhrushev <mfvanek@gmail.com>
2025-05-25 13:04:00 +0400

    Add check on cluster

---
+++ pg-index-health/src/test/java/io/github/mfvanek/pg/health/checks/cluster/PrimaryKeysThatMostLikelyNaturalKeysCheckOnClusterTest.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2019-2025. Ivan Vakhrushev and others.
+ * https://github.com/mfvanek/pg-index-health
+ *
+ * This file is a part of "pg-index-health" - a Java library for
+ * analyzing and maintaining indexes health in PostgreSQL databases.
+ *
+ * Licensed under the Apache License 2.0
+ */
+
+package io.github.mfvanek.pg.health.checks.cluster;
+
+import io.github.mfvanek.pg.core.checks.common.Diagnostic;
+import io.github.mfvanek.pg.core.fixtures.support.DatabaseAwareTestBase;
+import io.github.mfvanek.pg.core.fixtures.support.DatabasePopulator;
+import io.github.mfvanek.pg.health.checks.common.DatabaseCheckOnCluster;
+import io.github.mfvanek.pg.model.column.Column;
+import io.github.mfvanek.pg.model.context.PgContext;
+import io.github.mfvanek.pg.model.index.IndexWithColumns;
+import io.github.mfvanek.pg.model.predicates.SkipTablesByNamePredicate;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+
+import static io.github.mfvanek.pg.health.support.AbstractCheckOnClusterAssert.assertThat;
+
+class PrimaryKeysThatMostLikelyNaturalKeysCheckOnClusterTest extends DatabaseAwareTestBase {
+
+    private final DatabaseCheckOnCluster<IndexWithColumns> check = new PrimaryKeysThatMostLikelyNaturalKeysCheckOnCluster(getHaPgConnection());
+
+    @Test
+    void shouldSatisfyContract() {
+        assertThat(check)
+            .hasType(IndexWithColumns.class)
+            .hasDiagnostic(Diagnostic.PRIMARY_KEYS_THAT_MOST_LIKELY_NATURAL_KEYS)
+            .isStatic();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {PgContext.DEFAULT_SCHEMA_NAME, "custom"})
+    void onDatabaseWithThem(final String schemaName) {
+        executeTestOnDatabase(schemaName, DatabasePopulator::withNaturalKeys, ctx -> {
+            assertThat(check)
+                .executing(ctx)
+                .hasSize(3)
+                .containsExactly(
+                    IndexWithColumns.ofColumns(ctx, "t2_composite", "t2_composite_pkey", 0L,
+                        List.of(Column.ofNotNull(ctx, "t2_composite", "passport_series"), Column.ofNotNull(ctx, "t2_composite", "passport_number"))),
+                    IndexWithColumns.ofColumns(ctx, "t3_composite", "t3_composite_pkey", 0L,
+                        List.of(Column.ofNotNull(ctx, "t3_composite", "app_id"), Column.ofNotNull(ctx, "t3_composite", "app_number"))),
+                    IndexWithColumns.ofNotNull(ctx, "\"times-of-creation\"", "\"times-of-creation_pkey\"", "\"time-of-creation\"")
+                );
+
+            assertThat(check)
+                .executing(ctx, SkipTablesByNamePredicate.of(ctx, List.of("t2_composite", "t3_composite", "\"times-of-creation\"")))
+                .isEmpty();
+        });
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {PgContext.DEFAULT_SCHEMA_NAME, "custom"})
+    void onDatabaseWithoutThem(final String schemaName) {
+        executeTestOnDatabase(schemaName, DatabasePopulator::withReferences, ctx ->
+            assertThat(check)
+                .executing(ctx)
+                .isEmpty()
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {PgContext.DEFAULT_SCHEMA_NAME, "custom"})
+    void shouldWorkWithPartitionedTables(final String schemaName) {
+        executeTestOnDatabase(schemaName, DatabasePopulator::withVarcharInPartitionedTable, ctx ->
+            assertThat(check)
+                .executing(ctx)
+                .hasSize(1));
+    }
+}
