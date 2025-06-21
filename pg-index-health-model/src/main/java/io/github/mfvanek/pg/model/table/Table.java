b83769be7ac32639936a3e5fc5d848b59c0b906f
Ivan Vakhrushev <mfvanek@gmail.com>
2025-06-21 14:22:25 +0400

    Update copyright

--- pg-index-health-model/src/main/java/io/github/mfvanek/pg/model/table/Table.java
+++ pg-index-health-model/src/main/java/io/github/mfvanek/pg/model/table/Table.java
@@ -2,8 +2,8 @@
  * Copyright (c) 2019-2025. Ivan Vakhrushev and others.
  * https://github.com/mfvanek/pg-index-health
  *
- * This file is a part of "pg-index-health" - a Java library for
- * analyzing and maintaining indexes health in PostgreSQL databases.
+ * This file is a part of "pg-index-health" - an embeddable schema linter for PostgreSQL
+ * that detects common anti-patterns and promotes best practices.
  *
  * Licensed under the Apache License 2.0
  */
