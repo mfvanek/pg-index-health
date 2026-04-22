8e0c9b68ce9d7567f6959adde1e30c6f8e666ce4
Ivan Vakhrushev <mfvanek@gmail.com>
2026-04-22 09:37:55 +0400

    Add module-info.java

---
+++ pg-index-health-model/src/main/java/module-info.java
@@ -0,0 +1,18 @@
+module io.github.mfvanek.pg.model {
+    requires org.jspecify;
+
+    exports io.github.mfvanek.pg.model.annotations;
+    exports io.github.mfvanek.pg.model.bloat;
+    exports io.github.mfvanek.pg.model.column;
+    exports io.github.mfvanek.pg.model.constraint;
+    exports io.github.mfvanek.pg.model.context;
+    exports io.github.mfvanek.pg.model.dbobject;
+    exports io.github.mfvanek.pg.model.function;
+    exports io.github.mfvanek.pg.model.index;
+    exports io.github.mfvanek.pg.model.predicates;
+    exports io.github.mfvanek.pg.model.sequence;
+    exports io.github.mfvanek.pg.model.settings;
+    exports io.github.mfvanek.pg.model.table;
+    exports io.github.mfvanek.pg.model.units;
+    exports io.github.mfvanek.pg.model.validation;
+}
