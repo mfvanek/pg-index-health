dffd7a4fb468c3ea37bb98b9efb825c9dc55a840
Ivan Vakhrushev <mfvanek@gmail.com>
2026-09-05 13:45:36 +0400

    Add convention plugin for mandatory test-fixtures javadoc

---
+++ buildSrc/src/main/kotlin/pg-index-health.mandatory-test-fixtures-javadoc.gradle.kts
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2019-2026. Ivan Vakhrushev and others.
+ * https://github.com/mfvanek/pg-index-health
+ *
+ * This file is a part of "pg-index-health" - an embeddable schema linter for PostgreSQL
+ * that detects common anti-patterns and promotes best practices.
+ *
+ * Licensed under the Apache License 2.0
+ */
+
+plugins {
+    id("java")
+    id("java-test-fixtures")
+    id("pg-index-health.mandatory-javadoc")
+}
+
+tasks {
+    build {
+        dependsOn(testFixturesJavadoc)
+    }
+}
